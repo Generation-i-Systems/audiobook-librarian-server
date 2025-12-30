8e1e948de11ecb223e771f20bf8a3a1b4cde9796
Eric Thelin <eric.thelin@gmail.com>
2025-12-30 15:36:46 -0700

    fix(adminer): update Adminer controller and custom class
    Minor updates to Adminer integration for better functionality.
    
    - AdminerController improvements
    - Custom class enhancements for styling and behavior

--- app/Http/Controllers/Admin/AdminerController.php
+++ app/Http/Controllers/Admin/AdminerController.php
@@ -22,6 +22,7 @@ class AdminerController extends Controller
     public function handle(Request $request)
     {
         // Auto-populate connection details for the database
+        // This ensures Adminer starts with the right context
         if (!isset($_GET['username'])) {
             $_GET['username'] = config('database.connections.mysql.username');
         }
@@ -32,7 +33,7 @@ class AdminerController extends Controller
             $_GET['db'] = config('database.connections.mysql.database');
         }
         
-        // Ensure DRIVER is set
+        // Ensure DRIVER is set (defaults to server/mysql)
         if (!isset($_GET['server']) && !isset($_GET['sqlite']) && !isset($_GET['pgsql']) && !isset($_GET['oracle']) && !isset($_GET['mssql']) && !isset($_GET['mongo'])) {
             $_GET['server'] = config('database.connections.mysql.host');
         }
@@ -42,23 +43,29 @@ class AdminerController extends Controller
             define('SID', ''); 
         }
 
-        // Populate globals
+        // Populate globals. Adminer uses these heavily.
         $_GET = array_replace($_GET, $request->query->all());
         $_POST = array_replace($_POST, $request->request->all());
         $_SERVER = array_replace($_SERVER, $request->server->all());
 
-        // Define the adminer_object function in the global namespace
-        // It will be found by adminer.php even if it's namespaced
+        // Define the adminer_object function in the global namespace.
+        // Adminer (inside Adminer namespace) will find this global function.
         if (!function_exists('adminer_object')) {
+            /**
+             * This function is the standard Adminer hook for customization.
+             * It returns an instance of a class that extends Adminer.
+             */
             function adminer_object() {
-                // Include the class definition only when called
-                // At this point, the Adminer class will be defined by adminer.php
-                require_once base_path('resources/adminer/adminer-custom-class.php');
+                // We define the class inside the Adminer namespace to match adminer.php
+                if (!class_exists('Adminer\AdminerCustom')) {
+                    require_once base_path('resources/adminer/adminer-custom-class.php');
+                }
                 return new \Adminer\AdminerCustom;
             }
         }
 
         ob_start();
+        // Execute Adminer
         require base_path('resources/adminer/adminer.php');
         $output = ob_get_clean();
 
--- resources/adminer/adminer-custom-class.php
+++ resources/adminer/adminer-custom-class.php
@@ -2,13 +2,22 @@
 
 namespace Adminer;
 
+/**
+ * Custom Adminer class to handle auto-login using Laravel's configuration.
+ */
 class AdminerCustom extends Adminer
 {
+    /**
+     * Set the name displayed in the Adminer interface.
+     */
     function name()
     {
         return 'Librarian Database';
     }
 
+    /**
+     * Provide database credentials from Laravel's config.
+     */
     function credentials()
     {
         return [
@@ -18,16 +27,26 @@ class AdminerCustom extends Adminer
         ];
     }
 
+    /**
+     * Provide the default database name.
+     */
     function database()
     {
         return \config('database.connections.mysql.database');
     }
 
+    /**
+     * Automatic login: always return true.
+     * Access is already restricted to Laravel Admins via middleware.
+     */
     function login($login, $password)
     {
         return true;
     }
 
+    /**
+     * Hide the login form as it's not needed.
+     */
     function loginForm()
     {
         return "";
